3d457a4dff44c2a7595b15614171b59552210b9d
Aliyah <emailkamu@example.com>
2026-06-19 03:42:54 +0700

    h

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'cosmetics-api-production-05ca.up.railway.app/api',
+  baseURL: 'https://cosmetics-api-production-05ca.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
